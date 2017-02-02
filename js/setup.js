d9d393e2a848301e61def5fa9f02a1cca075b6a5
Yulia Dimina <myfri@yandex.ru>
2017-02-03 00:22:15 +0300

    Задание 13 Нажатие посохом. Предварительный вариант

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,14 @@
 'use strict';
+// находим контейнер формы настроек персонажа
 var setupContainer = document.querySelector('.setup');
-var setupOpen = document.querySelector('.setup-open');
-var setupClose = setupContainer.querySelector('.setup-close');
+// находим кнопку открытия контейнера
+var setupOpenBtn = document.querySelector('.setup-open-icon');
+// в контейнере формы настроек находим кнопку закрытия контейнера
+var setupCloseBtn = setupContainer.querySelector('.setup-close');
+// в контейнере формы настроек находим кнопку сохранения
+var setupSubmitBtn = setupContainer.querySelector('.setup-submit');
+// находим части svg-картинки персонажа, которые будем менять по клику
+// и задаем массивы цветов, которые будем применять по клику
 var wizardCoat = setupContainer.querySelector('#wizard-coat');
 var wizardCoatColors = [
   'rgb(101, 137, 164)',
@@ -28,29 +35,103 @@ var wizardFireballColors = [
   '#e6e848'
 ];
 var wizardFace = setupContainer.querySelector('#wizard-face');
-
-setupOpen.addEventListener('click', function () {
+// создаем константы для кнопок управления с клавиатуры enter и escape
+var BUTTON_BYENTER = 13;
+var BUTTON_CLOSE_BYESCAPE = 27;
+// задаем функцию открытия\закрытия контейнера формы (добавления\снятия соответствующего класса)
+var openSetupContainer = function () {
   setupContainer.classList.remove('invisible');
-});
-setupClose.addEventListener('click', function () {
+};
+var closeSetupContainer = function () {
   setupContainer.classList.add('invisible');
+};
+// задаем функцию отслеживания события клавиатуры "нажатие кнопки enter"
+var isActiveEvent = function (evt) {
+// узнаем, что Событие клавиатуры было && узнаем, что это именно Событие нажатия enter
+  return evt.keyCode && evt.keyCode === BUTTON_BYENTER;
+};
+// задаем функцию закрытия контейнера формы по Событию нажатия кнопки escape
+var setupCloseHendler = function (evt) {
+  if (evt.keyCode === BUTTON_CLOSE_BYESCAPE) {
+    closeSetupContainer();
+  }
+};
+// задаем функцию открытия контейнера формы и одновременного отслеживания нажатия escape
+// с последующим закрытием контейнера формы
+var setupOpenHendler = function () {
+  // создавали для снятия класса invisible
+  openSetupContainer();
+  // закрытие по Событию кнопки escape внутри открытого контейнера формы
+  document.addEventListener('keydown', setupCloseHendler);
+};
+
+// задаем функцию открытия контейнера формы настроек персонажа по КЛИКУ НА
+// кнопку открытия формы (сюда входит вся предыдущая колбасня с присвоением классов,
+// ослеживанием событий клавиатуры и прочими бубенцами)
+setupOpenBtn.addEventListener('click', function () {
+  setupOpenHendler();
 });
+// задаем функцию открытия контейнера формы настроек персонажа по НАЖАТИЮ НА
+// кнопку открытия формы (сюда входит вся предыдущая колбасня с присвоением классов,
+// ослеживанием событий клавиатуры и прочими бубенцами Плюс отслеживание события кнопки Enter)
+setupOpenBtn.addEventListener('keydown', function (evt) {
+  if (isActiveEvent(evt)) {
+    setupOpenHendler();
+  }
+});
+// тупо задаем функцию закрытия контейнера формы настроек по КЛИКУ НА кнопку закрытия
+setupCloseBtn.addEventListener('click', function () {
+  closeSetupContainer();
+});
+// не так тупо (уже с проверкой на Событие нажатия Enter) задаем функцию закрытия
+// контейнера формы настроек по НАЖАТИЮ НА кнопку закрытия
+setupCloseBtn.addEventListener('keydown', function (evt) {
+  if (isActiveEvent(evt)) {
+    closeSetupContainer();
+  }
+});
+// опять тупо задаем функцию закрытия контейнера формы настроек по КЛИКУ НА
+// кнопку сохранения настроек персонажа
+setupSubmitBtn.addEventListener('click', function () {
+  closeSetupContainer();
+});
+// опять но уже не так тупо (с проверкой на Событие кнопки Enter)
+// задаем функцию закрытия контейнера формы настроек по НАЖАТИЮ НА
+// кнопку сохранения настроек персонажа
+setupSubmitBtn.addEventListener('keydown', function (evt) {
+  if (isActiveEvent(evt)) {
+    closeSetupContainer();
+  }
+});
+// создаем колдунство с непредсказуемой сменой цветов по клику на (там в самом в начале
+// определенный нами) участок svg-картинки нашего персонажа
 wizardCoat.addEventListener('click', function () {
+  // записываем в переменную рандомный цвет, полученный с помощью хитровыстраданной
+  // функции (см. в самом конце) из массива цветов
   var coatColorRandom = getRandomNumber(wizardCoatColors.length);
+  // в стиль заливки svg выбранного участка (плащик в данном случае) записываем полученную переменную
+  // рандомного цвета - указываем ранее определенный массив (см в самом верху),
+  // а в кадратных скобках указываем рандомное значение из этого массива
   wizardCoat.style.fill = wizardCoatColors[coatColorRandom];
 });
+// Повторяем те же танцы с бубном вокруг остальных участков svg-картинки, которые ранее наметили
+// менять по клику
 wizardEyes.addEventListener('click', function () {
   var eyesColorRandom = getRandomNumber(wizardEyesColors.length);
   wizardEyes.style.fill = wizardEyesColors[eyesColorRandom];
 });
+
 wizardFireball.addEventListener('click', function () {
   var fireballColorsRandom = getRandomNumber(wizardFireballColors.length);
   wizardFireball.style.background = wizardFireballColors[fireballColorsRandom];
 });
+
 wizardFace.addEventListener('click', function () {
   wizardFace.style.fill = 'rgb(251, 216,' + Math.floor((Math.random() * 8) * 20) + ')';
 });
-
+// создаем функцию получения случайного значения из массивов всех ранее заданных цветов
 function getRandomNumber(maxNum) {
   return Math.floor(Math.random() * maxNum);
 }
+// удовлетворенно откидываемся, ждем когда наставник проверит и скажет, что
+// все к чегтавой бабушке переписать с нуля!
