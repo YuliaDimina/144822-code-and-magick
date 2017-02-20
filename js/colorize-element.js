5fbe39f79e2f162ca5e12f3f600e60cb4d450282
Yulia Dimina <myfri@yandex.ru>
2017-02-20 15:03:37 +0300

    Задание 19 финал

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,42 +1,6 @@
 'use strict';
 
-window.colorizeElement = function (callback) {
-  if (typeof callback === 'function') {
-    callback();
-  }
+window.colorizeElement = function (element, colors, callback) {
+  var newColorIndex = window.utils.getRandomElement(colors);
+  callback(element, colors[newColorIndex]);
 };
-
-window.utils.getRandomElementExcept();
-window.utils.isDeactivationEvent();
-window.utils.isActivationEvent();
-
-window.colorizeElement(function (element, colors, property) {
-  var setColor = function () {
-    var currentColor = element.style[property];
-    element.addEventListener('click', setNewColorHandle);
-    element.addEventListener('keydown', setNewColorHandle);
-    function setNewColorHandle(evt) {
-      if (evt.type === 'click' || evt.keyCode === window.utils.keyCods.BUTTON_BYENTER) {
-        currentColor = window.utils.getRandomElementExcept(colors, currentColor);
-        element.style[property] = colors[currentColor];
-      }
-    }
-  };
-  return setColor;
-})();
-
-
-// var catchPokemons = function(callback) {
-//   var started = Date.now();
-//   while (Date.now() - started < 100) {
-//     console.log('I\'m gonna get them all');
-//   }
-//
-//   if (typeof callback === 'function') {
-//     callback();
-//   }
-// };
-//
-// catchPokemons(function() {
-//   console.log('I\'m calling Karl');
-// });
